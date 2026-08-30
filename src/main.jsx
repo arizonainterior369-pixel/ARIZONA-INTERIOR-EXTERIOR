edbf0e0800e8384cbcd4b5392dce6d3add795cf6
arizonainterior369-pixel <arizonainterior369@gmail.com>
2026-08-30 15:07:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
-
+import logo from "./assets/arizona-logo.svg";
 function App() {
   return (
     <>
